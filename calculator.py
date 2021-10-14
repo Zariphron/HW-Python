1ddda70fb2a12e84f10452e0d4a5484fa6451f3b
Zariphron <hawkwield8397.4@gmail.com>
2021-10-14 16:05:23 -0700

    Initial commit for calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def calculator(number1, number2, operator):
+    result = 0
+    if ((type(number1) not float or int) or (type(number2) not float or int)):
+        False
+    if (operator is "+"):
+        result = number1 + number2
+    if (operator is "-"):
+        result = number1 - number2
+    if (operator is "*"):
+        result = number1 * number2
+    if (operator is "/"):
+        result = number1 / number2
+    if (operator is "//"):
+        result = 
+    if (operator is "**"):
+        result = 
+    else:
+        False
+
+def parse_input():
+    something
