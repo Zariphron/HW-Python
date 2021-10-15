5a94ffd80decee2e43498f1b5f2002b76a699448
Zariphron <hawkwield8397.4@gmail.com>
2021-10-14 22:12:52 -0700

    Gave a variable to sleep the system

--- timing.py
+++ timing.py
@@ -1,14 +1,14 @@
 import time
 
-def calculate_time():
+def calculate_time(delay):
     """
     This is designed to count the amount of time
     it takes for this specific function to run in seconds.
     """
     initial_time = time.time()
     # Any other actions would go here to get an accurate measurement of how long this function takes.
+    time.sleep(delay)
     final_time = time.time()
     X = final_time - initial_time
     print(f"Total time {X}")
 
-calculate_time()
