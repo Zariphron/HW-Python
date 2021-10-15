833e5e7c5fb79a46d2f2d2400856a1227e51286f
Zariphron <hawkwield8397.4@gmail.com>
2021-10-14 20:32:20 -0700

    Provided comments and fine tuned the code

--- calculator.py
+++ calculator.py
@@ -1,23 +1,33 @@
 def parse_input():
+    """
+    This will take what the user inputs and transform it
+    into something the calculator function can recognize.
+    """
     user_input = input("Enter equation: ")
     user_input = user_input.split(" ")
     calculator(float(user_input[0]), float(user_input[2]), user_input[1])
 
 def calculator(number1, number2, operator):
+    """
+    This function will take input from the user and return an output
+    based on the user's operation input.
+
+    The operator can be: '+','-','*','/','**', or '//'
+    
+    """
     result = 0
     if (operator == "+"):
-        result = number1 + number2
+        return result = number1 + number2
     if (operator == "-"):
-        result = number1 - number2
+        return result = number1 - number2
     if (operator == "*"):
-        result = number1 * number2
+        return result = number1 * number2
     if (operator == "/"):
-        result = number1 / number2
+        return result = number1 / number2
     if (operator == "//"):
-        result = number1 // number2
+        return result = number1 // number2
     if (operator == "**"):
-        result = number1 ** number2
+        return result = number1 ** number2
+    # In the event that the operator variable is not valid, the function will end and give a 'false' output.
     else:
-        False
-
-parse_input()
+        return False
--- multiply.py
+++ multiply.py
@@ -1,8 +1,33 @@
 def multiply_list(num_List):
+    """
+    This will take in a list and multiply
+    each number by the previous number in the list
+    continuously until it reaches the end of the list.
+
+    The end result will be all numbers multiplied together.
+
+    Example:
+    [2,3,4]
+    Because 2*3*4
+    Will end up being = 24
+
+    """
     result = 1
+    # The result cannot be non-initialized otherwise it will return an error.
     for i in num_List:
         if type(i) == int or float:
             result = result * i
         else:
-            False
-    result
+            return False
+    return result
+
+"""def parse_input():
+    user_numbers = []
+    string_input = input("Input = ")
+    string_input = string_input.replace("[", "")
+    string_input = string_input.replace("]", "")
+    string_input = string_input.split(",")
+    for i in string_input:
+        user_numbers.append(int(i))
+    multiply_list(user_numbers)
+"""
